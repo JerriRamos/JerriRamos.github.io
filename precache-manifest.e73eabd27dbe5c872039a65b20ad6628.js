df7f8327a700d2170c14c09a5542e8bde7735d7c
J. C. R <66345506+JerriRamos@users.noreply.github.com>
2020-07-29 17:43:06 -0100

    Deploy of My Portfólio

---
+++ precache-manifest.e73eabd27dbe5c872039a65b20ad6628.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3d3c7d614a6e02700d9efb3c7a4f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7974fcfc0465fba8129",
+    "url": "/static/css/main.03752297.chunk.css"
+  },
+  {
+    "revision": "e68776750df16a9c0fa0",
+    "url": "/static/js/2.aaf7b80c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aaf7b80c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7974fcfc0465fba8129",
+    "url": "/static/js/main.6c0a7eb0.chunk.js"
+  },
+  {
+    "revision": "b936188c35c71ef0b95c",
+    "url": "/static/js/runtime-main.11c9a03d.js"
+  },
+  {
+    "revision": "b08b354e3936b3cdc6408b15557b5219",
+    "url": "/static/media/a1.b08b354e.png"
+  },
+  {
+    "revision": "018c6b7011923bc805fae87d0f8e7c64",
+    "url": "/static/media/b1.018c6b70.png"
+  },
+  {
+    "revision": "8af5c5bbdce74f690c08acc634cc6015",
+    "url": "/static/media/comingSoon.8af5c5bb.jpg"
+  },
+  {
+    "revision": "56ba4951d600f6c5650ead9159579245",
+    "url": "/static/media/comingSoon1.56ba4951.jpg"
+  },
+  {
+    "revision": "840786b48903dcfc27a0d828c4a14357",
+    "url": "/static/media/comingSoon2.840786b4.jpg"
+  },
+  {
+    "revision": "b088495781ff3d11132025251915ed9f",
+    "url": "/static/media/comingSoon3.b0884957.jpg"
+  },
+  {
+    "revision": "0369bec1581327f79d09da3a6bd0d67e",
+    "url": "/static/media/db.0369bec1.png"
+  },
+  {
+    "revision": "212b4c6beb8f499da063e308659c1d42",
+    "url": "/static/media/img1.212b4c6b.jpg"
+  },
+  {
+    "revision": "3b49982a78fef66faa3efb7a85ac2abc",
+    "url": "/static/media/img2.3b49982a.jpg"
+  },
+  {
+    "revision": "b3c637b234baebc0fa0b6d178594b7e1",
+    "url": "/static/media/mainPic.b3c637b2.jpg"
+  }
+]);
